09d57b1ebbc0c76d56e472fa9d5e87a6ce745348
Sudo Dios <sdidevelop.work@gmail.com>
2023-02-03 01:06:55 +0330

    Update paddingMethod to api 16

--- app/src/main/java/me/sudodios/particlerainapp/MainActivity.kt
+++ app/src/main/java/me/sudodios/particlerainapp/MainActivity.kt
@@ -2,10 +2,7 @@ package me.sudodios.particlerainapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePaddingRelative
+import androidx.core.view.*
 import me.sudodios.particlerainapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-            binding.laySeeks.updatePaddingRelative(bottom = insets.bottom)
+            binding.laySeeks.updatePadding(left = 0, right = 0, top = 0, bottom = insets.bottom)
             WindowInsetsCompat.CONSUMED
         }
 
@@ -27,10 +24,10 @@ class MainActivity : AppCompatActivity() {
         binding.colorSeekbar.onColorChanged = {
             binding.particleView.color = it
         }
-        binding.speedSeekbar.addOnChangeListener { slider, fl, b ->
+        binding.speedSeekbar.addOnChangeListener { _, fl, _ ->
             binding.particleView.speed = fl
         }
-        binding.maxCountSeekbar.addOnChangeListener { slider, fl, b ->
+        binding.maxCountSeekbar.addOnChangeListener { _, fl, _ ->
             binding.particleView.maxCount = fl.toInt()
         }
     }
