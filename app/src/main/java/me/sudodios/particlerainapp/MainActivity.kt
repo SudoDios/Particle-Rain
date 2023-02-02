ecf37964ee786ba2671564380a24a93f47364b69
Sudo Dios <sdidevelop.work@gmail.com>
2023-02-03 00:37:33 +0330

    Updated app preview

--- app/src/main/java/me/sudodios/particlerainapp/MainActivity.kt
+++ app/src/main/java/me/sudodios/particlerainapp/MainActivity.kt
@@ -2,7 +2,10 @@ package me.sudodios.particlerainapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePaddingRelative
 import me.sudodios.particlerainapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         WindowCompat.setDecorFitsSystemWindows(window, false)
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { _, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            binding.laySeeks.updatePaddingRelative(bottom = insets.bottom)
+            WindowInsetsCompat.CONSUMED
+        }
 
+
+        binding.colorSeekbar.onColorChanged = {
+            binding.particleView.color = it
+        }
+        binding.speedSeekbar.addOnChangeListener { slider, fl, b ->
+            binding.particleView.speed = fl
+        }
+        binding.maxCountSeekbar.addOnChangeListener { slider, fl, b ->
+            binding.particleView.maxCount = fl.toInt()
+        }
     }
 }
